fae08d101321d1e17ca7925e60c4da6a82d9a941
luckyvan <fanchou1215@gmail.com>
2018-01-07 21:37:53 +0800

    1.2.3 Rotation

---
+++ Assets/Scripts/RotationTransformation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RotationTransformation : Transformation
+{
+
+    public Vector3 rotation;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        float radZ = rotation.z * Mathf.Deg2Rad;
+        var sinZ = Mathf.Sin(radZ);
+        var cosZ = Mathf.Cos(radZ);
+
+        //x(1,0) + y(0,1) --> x(cosZ, sinZ) + y(-sinZ, cosZ)
+        return new Vector3(
+            point.x * cosZ - point.y * sinZ,
+            point.x * sinZ + point.y * cosZ,
+            point.z);
+    }
+}
